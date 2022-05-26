98aeda22c2fe0d31a7039dbc833fb5d2eafa0093
bnv20 <amicobnv20@gmail.com>
2022-05-26 15:03:35 +0900

    태그 만들기

--- cakd_dj/blog/models.py
+++ cakd_dj/blog/models.py
@@ -2,6 +2,15 @@ import os
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -29,6 +38,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    tags = models.ManyToManyField(Tag, blank=True)
    
     # author: 추후 작성 예정
     def __str__(self):
